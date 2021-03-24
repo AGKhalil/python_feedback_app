e73c98294d1d28ff9f0661d75abca2cd4c8fc9bf
AGKhalil <elghazalykhalil@gmail.com>
2021-03-24 19:57:39 +0200

    yet another final maybe

--- app.py
+++ app.py
@@ -11,8 +11,8 @@ if ENV == 'dev':
     app.config['SQLALCHEMY_DATABASE_URI'] = ''
 else:
     app.debug = False
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://mpusiedmvpvnnx:f28ef0c11d6af66e53788beee0b820910c7e9682e5ddaf8fc9ffe1d58e48070c@ec2-54-235-
-108-217.compute-1.amazonaws.com:5432/dcv0k4q2k6ues0'
+    app.config[
+        'SQLALCHEMY_DATABASE_URI'] = 'postgres://mpusiedmvpvnnx:f28ef0c11d6af66e53788beee0b820910c7e9682e5ddaf8fc9ffe1d58e48070c@ec2-54-235-108-217.compute-1.amazonaws.com:5432/dcv0k4q2k6ues0'
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
@@ -48,14 +48,17 @@ def submit():
         comments = request.form['comments']
         # print(customer, dealer, rating, comments)
         if customer == '' or dealer == '':
-            return render_template('index.html', message='Please enter required fields')
-        if db.session.query(Feedback).filter(Feedback.customer == customer).count() == 0:
+            return render_template('index.html',
+                                   message='Please enter required fields')
+        if db.session.query(Feedback).filter(
+                Feedback.customer == customer).count() == 0:
             data = Feedback(customer, dealer, rating, comments)
             db.session.add(data)
             db.session.commit()
             send_mail(customer, dealer, rating, comments)
             return render_template('success.html')
-        return render_template('index.html', message='You have already submitted feedback')
+        return render_template('index.html',
+                               message='You have already submitted feedback')
 
 
 if __name__ == '__main__':
