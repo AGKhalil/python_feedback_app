33f57c5cf231e35f19706116461ec254e9beec12
AGKhalil <elghazalykhalil@gmail.com>
2021-03-27 20:44:35 +0200

    trying webhooks 2

--- app.py
+++ app.py
@@ -51,7 +51,7 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/transaction:sale:read', methods=['POST'])
+@app.route('/transaction/sale/read', methods=['POST'])
 def respond():
     print_to_stdout("FUUUUCK")
     return render_template('webhook.html', variable="FUCK")
